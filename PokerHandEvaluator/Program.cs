b6a068a16561725ff75282b7f2aa4725a24ad688
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2018-06-30 12:48:31 +0200

    Add project files.

---
+++ PokerHandEvaluator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PokerHandEvaluator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
