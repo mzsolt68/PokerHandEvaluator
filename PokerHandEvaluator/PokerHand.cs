ed7e4cfc738dcaa0bdc7fa4d9d4c74eb1ed30df0
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2018-06-30 13:10:17 +0200

    A PokerHand osztályhoz hozzáadva: - konstruktor - Sort metódus a lapok növekvő sorbarendezéséhez - az IComparable interfész implementációja

--- PokerHandEvaluator/PokerHand.cs
+++ PokerHandEvaluator/PokerHand.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace PokerHandEvaluator
@@ -10,6 +11,7 @@ namespace PokerHandEvaluator
     {
         public RankType Rank { get; private set; }
         public SuitType Suit { get; private set; }
+
         public Card(RankType rank, SuitType suit) : this()
         {
             Rank = rank;
@@ -17,7 +19,36 @@ namespace PokerHandEvaluator
         }
     }
 
-    class PokerHand
+    public class PokerHand : IComparable<PokerHand>
     {
+        public Card[] Cards { get; private set; }
+
+        public PokerHand(Card c1, Card c2, Card c3, Card c4, Card c5)
+        {
+            Cards = new Card[]{c1, c2, c3, c4, c5};
+            Sort();
+        }
+
+        private void Sort()
+        {
+            Cards = Cards.OrderBy(c => c.Rank).OrderBy(c => Cards.Where(c1 => c1.Rank == c.Rank).Count()).ToArray();
+
+            if (Cards[4].Rank == RankType.Ace && Cards[0].Rank == RankType.Two && (int)Cards[3].Rank - (int)Cards[0].Rank == 3)
+                Cards = new Card[] { Cards[4], Cards[0], Cards[1], Cards[2], Cards[3] };
+        }
+
+        public int CompareTo(PokerHand other)
+        {
+            for (int i = 4; i >= 0; i--)
+            {
+                RankType rank1 = Cards[i].Rank;
+                RankType rank2 = other.Cards[i].Rank;
+                if (rank1 > rank2)
+                    return 1;
+                if (rank1 < rank2)
+                    return -1;
+            }
+            return 0;
+        }
     }
 }
