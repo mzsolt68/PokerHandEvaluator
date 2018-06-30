e82aaaa5ad75d2501f4d02b8e2bf15e0dc66215d
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2018-06-30 12:56:13 +0200

    PokerHand osztály létrehozva. A lapok értékét és színét leíró enum-ok és az egy káryalapot leíró struct létrehozva

---
+++ PokerHandEvaluator/PokerHand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokerHandEvaluator
+{
+    public enum RankType : int { Two = 2, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace}
+    public enum SuitType : int { Spades, Hearts, Diamonds, Clubs}
+    public struct Card
+    {
+        public RankType Rank { get; private set; }
+        public SuitType Suit { get; private set; }
+        public Card(RankType rank, SuitType suit) : this()
+        {
+            Rank = rank;
+            Suit = suit;
+        }
+    }
+
+    class PokerHand
+    {
+    }
+}
