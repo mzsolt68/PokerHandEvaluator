f15446ca70005b0b5a5846ddd35fe654088bffc8
mlatilik.zsolt <mzsolt68@users.noreply.github.com>
2018-07-01 19:36:37 +0200

    Teszt adatok létrehozása a főprogramban.

--- PokerHandEvaluator/Program.cs
+++ PokerHandEvaluator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PokerHandEvaluator
 {
@@ -7,6 +8,31 @@ namespace PokerHandEvaluator
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Dictionary<string, PokerHand> hands = new Dictionary<string, PokerHand>();
+            PokerHand hand1 = new PokerHand
+                (
+                new Card(RankType.Ace, SuitType.Spades),
+                new Card(RankType.King, SuitType.Hearts),
+                new Card(RankType.Ten, SuitType.Hearts),
+                new Card(RankType.Ten, SuitType.Spades),
+                new Card(RankType.Ace, SuitType.Hearts)
+                );
+            PokerHand hand2 = new PokerHand
+                (
+                new Card (RankType.Ace, SuitType.Clubs ),
+                new Card ( RankType.Nine, SuitType.Clubs ),
+                new Card ( RankType.Ten, SuitType.Clubs ),
+                new Card ( RankType.Two, SuitType.Clubs ),
+                new Card ( RankType.Seven, SuitType.Clubs )
+                );
+            hands.Add("hand1", hand1);
+            hands.Add("hand2", hand2);
+            foreach (var player in PokerHand.Evaluate(hands))
+            {
+                Console.WriteLine(player);
+            }
+
+            Console.Read();
         }
     }
 }
